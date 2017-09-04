78f498c8a9917b4f125fe2b05d55210b342b7291
potterve <potterve@amazon.com>
2017-09-04 10:05:54 -0600

    force lowercase on the cognito app integration domain name.

--- dashboard-app/build/update-dashboard-config.js
+++ dashboard-app/build/update-dashboard-config.js
@@ -15,7 +15,7 @@ stream.once('open', function(fd) {
   stream.write('var appUserPoolClientId = \'' + process.env.USER_POOL_CLIENT_ID + '\';\n');
   stream.write('var appIdentityPoolId = \'' + process.env.IDENTITY_POOL_ID + '\';\n');
   stream.write('var appRegion = \'' + process.env.AWS_DEFAULT_REGION + '\';\n');
-  stream.write('var appDomainName = \'' + process.env.APP_DOMAIN_NAME + '\';\n');
+  stream.write('var appDomainName = \'' + process.env.APP_DOMAIN_NAME + '\'.toLowerCase();\n');
   stream.write('var appUserPoolName = \'' + process.env.USER_POOL_NAME + '\';\n');
   stream.end();
 });
