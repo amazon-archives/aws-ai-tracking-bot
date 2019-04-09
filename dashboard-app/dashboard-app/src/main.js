f14ff30e05180818de0a838263cdba439653e89c
James Siri <jamesiri@amazon.com>
2019-04-09 10:36:39 -0700

    Update main.js

--- dashboard-app/dashboard-app/src/main.js
+++ dashboard-app/dashboard-app/src/main.js
@@ -1,7 +1,7 @@
 /* eslint-disable max-len */
 
 /*
-# Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# Copyright 2017-2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: MIT-0.
  */
 
